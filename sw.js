44f53671d166d6755452830dad4fc0c2738d2aa8
N3rdyN8 <127324885+N3rdyN8@users.noreply.github.com>
2026-01-01 18:26:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,18 @@
 const CACHE = "action-poker-v1";
-
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./sw.js",
   "./icon-192.png",
-  "./icon-512.png",
-  "./apple-touch-icon.png"
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
